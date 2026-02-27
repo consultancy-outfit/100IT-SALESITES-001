c49102c01b4909a244393c4828db1bda0e423a6f
mazamaltaf <muhammadazam.altaf@consultancyoutfit.co.uk>
2026-02-27 16:39:56 +0500

    Update constants.ts
    contact information update

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const COMPANY_DETAILS = {
   name: "Oaklands Respite",
-  address: "124 City Road, London, EC1V 2NX, United Kingdom",
-  crn: "08456789",
-  phone: "+44 20 7946 0000",
+  address: "United Kingdom",
+  crn: "000000",
+  phone: "+44 000000000",
   email: "Info@oaklandsrespite.co.uk",
   hours: "Mon - Fri: 09:00 - 18:00",
 };
