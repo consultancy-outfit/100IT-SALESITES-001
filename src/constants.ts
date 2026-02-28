105b2a367054adbadb2e739b9d543b1a04be25da
Adnan-tariq-outfit <adnan.tariq@consultancyoutfit.co.uk>
2026-02-28 08:26:07 +0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const COMPANY_DETAILS = {
   name: "Oaklands Respite",
-  address: "United Kingdom",
-  crn: "000000",
-  phone: "+44 000000000",
-  email: "Info@oaklandsrespite.co.uk",
+  address: "1 Highfield Love Lane, Kings Langley, England, WD4 9JT",
+  crn: "17050672",
+  phone: "0170 640 7196",
+  email: "info@oaklandsrespite.co.uk",
   hours: "Mon - Fri: 09:00 - 18:00",
 };
