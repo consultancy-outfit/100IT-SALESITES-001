6b8febf030c41b91c15c58f5da841c14995055ce
unknown <adnan.tariq@consultancyoutfit.co.uk>
2026-03-09 14:17:44 +0500

    add logo

---
+++ src/ScrollToTop.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Always jump to the very top on route change
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
